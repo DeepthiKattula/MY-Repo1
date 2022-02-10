7425c517a8013e01241215fde4aba4f76f6b123e
DeepthiKattula <98301057+DeepthiKattula@users.noreply.github.com>
2022-02-10 19:34:01 +0530

    Add files via upload

---
+++ Exceptions/src/exceptions/FinallyKeyword.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+public class FinallyKeyword {
+	public static void main(String[] args)
+    {
+        int a=45,b=6,rs=0;
+        try
+        {
+            rs = a / b;
+        }
+        catch(ArithmeticException Ex)
+        {
+            System.out.print("\n\tError : " + Ex.getMessage());
+        }
+        finally
+        {
+            System.out.print("\n\tThe result is : " + rs);
+        }
+    }
+
+}
