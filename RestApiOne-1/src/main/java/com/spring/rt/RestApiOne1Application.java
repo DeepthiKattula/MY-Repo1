63ad69cde324184accca30d0794fbfd0dc7b10f0
DeepthiKattula <98301057+DeepthiKattula@users.noreply.github.com>
2022-06-08 15:52:10 +0530

    Add files via upload

---
+++ RestApiOne-1/src/main/java/com/spring/rt/RestApiOne1Application.java
@@ -0,0 +1,13 @@
+package com.spring.rt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiOne1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiOne1Application.class, args);
+	}
+
+}
